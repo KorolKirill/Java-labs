fe4dd88de313ba2416171c3c744c20c6e2c88bd5
Korol Kirill <71098249+KorolKirill@users.noreply.github.com>
2022-09-27 23:10:13 +0300

    lab 8 is completed and lab3 is finished
    lab 8 is completed and lab3 is finished

--- lab3/src/Main.java
+++ lab3/src/Main.java
@@ -1,32 +1,114 @@
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<Shape> randomShapes = Generator.generateRandomShapes(10);
+        View view = new LabView();
+        LabModel labModel = new LabModel();
+        LabController controller = new LabController(view, labModel);
+        controller.completeLab();
+    }
+}
 
+class LabController {
+    private View view;
+    private LabModel model;
+
+    public LabController(View view, LabModel model) {
+        this.view = view;
+        this.model = model;
+    }
+
+    public void completeLab() {
+        model.generateShapes(15);
         //відображає набір даних;
-        randomShapes.forEach(System.out::println);
+        view.showData(model.getShapes()
+                .stream()
+                .map(x->x.toString())
+                .toList());
+
         //обчислює сумарну площу всіх фігур набору даних;
-        double totalArea = randomShapes.stream()
-                .map(x-> x.calcArea())
-                .reduce(0d,((a, b) -> a + b ));
-        System.out.println(totalArea);
+        var totalArea =  model.getTotalArea();
+        view.showMessage("Total area is: " + totalArea);
+        //обчислює сумарну площу фігур заданого виду;
+        var totalAreaFiltered = model.totalAreaByType(Circle.class);
+        view.showMessage("Total area filtered by Circle class: " + totalAreaFiltered);
+        //впорядковує набір даних щодо збільшення площі фігур, використовуючи об'єкт інтерфейсу Comparator;
+        model.sortByArea();
+        view.showData(model.getShapes().stream().map(x->x.toString()).toList());
+        //впорядковує набір даних за кольором фігур, використовуючи об'єкт інтерфейсу Comparator.
+        model.sortByColor();
+        view.showData(model.getShapes().stream().map(x->x.toString()).toList());
+
+        // completed
+        view.showMessage("Lab is completed.");
+    }
+}
+
+interface View {
+    void showMessage(String message);
+    void showData(Iterable<String> iterable);
+}
+
+class LabView implements View {
+    public void showMessage(String message) {
+        System.out.println("System");
+    }
+
+    @Override
+    public void showData(Iterable<String> iterable) {
+        iterable.forEach(System.out::println);
+    }
+}
+
+class LabModel {
+    List<Shape> randomShapes;
+
+    public LabModel() {
+        this.randomShapes = generateShapes(10);
+    }
+
+    public List<Shape> generateShapes(int amount) {
+        return  Generator.generateRandomShapes(amount);
+    }
+
+    public void setShapesList(List<Shape> shapes) {
+        this.randomShapes = shapes;
+    }
+
+    public void sortByArea() {
+        //впорядковує набір даних щодо збільшення площі фігур, використовуючи об'єкт інтерфейсу Comparator;
+        Comparator<Shape> compareByArea = (x,y) -> (x.calcArea() > y.calcArea()) ? 1 : 0;
+        randomShapes.sort(compareByArea);
+    }
+
+    public void sortByColor() {
+        // впорядковує набір даних за кольором фігур, використовуючи об'єкт інтерфейсу Comparator.
+        Comparator<Shape> compareByColors = (x,y) -> x.shapeColor.compareTo(y.shapeColor);
+        randomShapes.sort(compareByColors);
+    }
+
+    public double totalAreaByType(Type type) {
         // обчислює сумарну площу фігур заданого виду;
         double totalAreaByType = randomShapes.stream()
                 .filter(x-> x.getClass() == Circle.class)
                 .map(Shape::calcArea)
                 .reduce(0d,((a, b) -> a + b ));
-        System.out.println(totalAreaByType);
-        //впорядковує набір даних щодо збільшення площі фігур, використовуючи об'єкт інтерфейсу Comparator;
-        Comparator<Shape> compareByArea = (x,y) -> (x.calcArea() > y.calcArea()) ? 1 : 0;
-        randomShapes.sort(compareByArea);
-        System.out.println(randomShapes);
-        // впорядковує набір даних за кольором фігур, використовуючи об'єкт інтерфейсу Comparator.
-        Comparator<Shape> compareByColors = (x,y) -> x.shapeColor.compareTo(y.shapeColor);
-        randomShapes.sort(compareByColors);
-        System.out.println(randomShapes);
+      return totalAreaByType;
+    }
+
+    public double getTotalArea() {
+        //обчислює сумарну площу всіх фігур набору даних;
+        double totalArea = randomShapes.stream()
+                .map(x-> x.calcArea())
+                .reduce(0d,((a, b) -> a + b ));
+        return totalArea;
+    }
+
+    public List<Shape> getShapes() {
+        return  randomShapes;
     }
 }
 
@@ -51,6 +133,9 @@ class Generator {
             }
         }
         return randomShapes;
+    }
+    static String getRandomColor() {
+        return  colors[(int) Math.floor(Math.random()*colors.length)];
     }
 }
 
---
+++ lab8/src/Main.java
@@ -0,0 +1,105 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        ParallelMonteCarloPi calc = new ParallelMonteCarloPi();
+        long timeStart = 0L;
+        System.out.println("Started: ");
+
+        timeStart = System.currentTimeMillis();
+        int thCount = 8;
+        long iterations = 100_000_000l;
+        double res = calc.findPI(iterations, thCount);
+        long time = System.currentTimeMillis() - timeStart;
+        System.out.println(
+                "PI is : " + res +
+                "\n THREADS  " + thCount +
+                "\n ITERATIONS  " + iterations +
+                        "\n TIME   " + time + " ms"
+        );
+
+    }
+}
+
+class ParallelMonteCarloPi {
+
+    private class Point {
+        double x;
+        double y;
+
+        public Point(double x, double y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public double calculateLength() {
+            return Math.sqrt(x * x + y * y);
+        }
+    }
+
+    public double findPI(long iterations, int threadsCount) throws Exception {
+        if (threadsCount <= 0 || iterations <= 0) {
+            throw new Exception("Wrong params.");
+        }
+
+        //множители for single
+        long iterationSingle = (iterations / threadsCount);
+
+        List<ArithmeticThread> threads = new ArrayList<ArithmeticThread>(threadsCount);
+
+        ArithmeticThread at;
+        for (int i = 0; i < threadsCount; i++) {
+            at = new ArithmeticThread(iterationSingle);
+            threads.add(at);
+            at.start();
+        }
+
+        double result = 0;
+        for (ArithmeticThread thread : threads) {
+            try {
+                thread.join();
+                result = result == 0 ? thread.getResult() : (result + thread.getResult()) / 2 ;
+                System.out.println(thread.getResult());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // ⦁	Частка точок, які потрапили в коло дорівнює наближеному значенню π/4
+        return result * 4;
+    }
+
+    private class ArithmeticThread extends Thread {
+        double result;
+        long iterations;
+
+        public ArithmeticThread(long iterations) {
+            this.iterations = iterations;
+
+        }
+
+        @Override
+        public void run() {
+//            averageResult = 0;
+//            for (long i = 1; i <= this.iterations; i++) {
+//                Point point = new Point(Math.random(), Math.random());
+//                averageResult = (averageResult + point.calculateLength()) / i;
+//                ;
+//            }
+            long counterSuccess = 0;
+            for (long i = 1; i <= this.iterations; i++) {
+                Point point = new Point(Math.random(), Math.random());
+                if (point.calculateLength() <= 1) {
+                    counterSuccess++;
+
+                }
+            }
+            result = (double) counterSuccess / iterations;
+        }
+
+        public double getResult() {
+            return result;
+        }
+    }
+}
